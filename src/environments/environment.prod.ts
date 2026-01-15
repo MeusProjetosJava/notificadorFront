d29f5892f773c8dca26d0a834dc54c9878756621
Vitor <vitorcostaofficial@gmail.com>
2026-01-15 10:36:05 -0300

    add https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://notificador-backend-production.up.railway.app',
+  apiUrl: 'https://notificador-backend-production.up.railway.app',
 };
